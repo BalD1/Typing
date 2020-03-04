96aa93909136c9d481283a1af7804a4b39eb626a
Allan <couetteallan@gmail.com>
2020-03-04 16:35:46 +0100

    Variables oui
    public int oui = 42;

---
+++ Typing/Assets/Scripts/Oui.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oui : MonoBehaviour
+{
+    public int oui = 42;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
