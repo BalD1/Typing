2753f9cfe8535667b20e3699e41f5829c9d9b9bf
Allan <couetteallan@gmail.com>
2020-03-04 17:23:42 +0100

    La création de Billy
    mouvement et rigidbody de billy

---
+++ Typing/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int speed = 30;
+
+
+    Rigidbody2D billy2d;
+
+    void Start()
+    {
+        this.billy2d = this.GetComponent<Rigidbody2D>();
+
+    }
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = billy2d.position;
+        position.x = position.x + this.speed * horizontal * Time.deltaTime;
+        position.y = position.y + this.speed * vertical * Time.deltaTime;
+
+        this.billy2d.MovePosition(position);
+    }
+}
