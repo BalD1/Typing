8043c5437aaa238f8259aa44afc984be7dd50106
etiehuman <55735185+etiehuman@users.noreply.github.com>
2020-03-10 09:34:01 +0100

    Fusion de EnemyFollow et Ennemi
    Fusion de EnemyFollow et Ennemi

--- Typing/Assets/Scripts/Ennemi.cs
+++ Typing/Assets/Scripts/Ennemi.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ennemi : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
+
+
 }
