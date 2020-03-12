c29e3faf73a0b7b1d442971fd773d3d8fbc1a8de
Florian <florianhusson@hotmail.fr>
2020-03-12 12:01:20 +0100

    Le menu v.01

---
+++ Typing/Assets/Scripts/MasterButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterButton : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool Active;
+    public string ButtonType;
+
+    [SerializeField]
+    GameObject button1;
+    [SerializeField]
+    GameObject button2;
+
+    void Awake()
+    {
+        button1.SetActive(false);
+        button2.SetActive(false);
+    }
+
+    public void SetActiveButton(bool State)
+    {
+        button1.SetActive(State);
+        button2.SetActive(State);
+    }
+
+}
