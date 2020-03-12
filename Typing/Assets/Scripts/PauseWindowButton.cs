b1e1a65dc4545dd454afdf0465b1485eaa9c3784
Florian <florianhusson@hotmail.fr>
2020-03-12 16:53:05 +0100

    Le menu et l'écran pause (échap)

---
+++ Typing/Assets/Scripts/PauseWindowButton.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseWindowButton : MonoBehaviour
+{
+    private List<GameObject> GrimoireList = new List<GameObject>();
+    private List<string> LearnedSpells = new List<string>();
+    [SerializeField]
+    private GameObject GrimoireEau;
+    [SerializeField]
+    private GameObject GrimoireGlace;
+    [SerializeField]
+    private GameObject GrimoireVampirisme;
+    [SerializeField]
+    private GameObject GrimoireVent;
+    [SerializeField]
+    private GameObject GrimoireBouclier;
+    [SerializeField]
+    private GameObject GrimoireFoudre;
+    [SerializeField]
+    private GameObject GrimoirePower;
+    [SerializeField]
+    private GameObject GrimoireSoin;
+    
+    
+
+    private void Start()
+    {
+        GrimoireList.Add(GrimoireEau);
+        GrimoireList.Add(GrimoireGlace);
+        GrimoireList.Add(GrimoireVampirisme);
+        GrimoireList.Add(GrimoireVent);
+        GrimoireList.Add(GrimoireBouclier);
+        GrimoireList.Add(GrimoireFoudre);
+        GrimoireList.Add(GrimoirePower);
+        GrimoireList.Add(GrimoireSoin);
+
+        foreach(GameObject grimoire in GrimoireList)
+        {
+            grimoire.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        this.LearnedSpells = GameManager.Instance.SendLearnedSpellList();
+        foreach(string learnedSpell in LearnedSpells)
+        {
+            UnlockingSpell(learnedSpell);
+        }
+    }
+
+    private void UnlockingSpell(string UnlockedSpell)
+    {
+        switch (UnlockedSpell)
+        {
+            case "eau":
+                GrimoireList[0].SetActive(true);
+                break;
+            case "glace":
+                GrimoireList[1].SetActive(true);
+                break;
+            case "vampirisme":
+                GrimoireList[2].SetActive(true);
+                break;
+            case "vent":
+                GrimoireList[3].SetActive(true);
+                break;
+            case "bouclier":
+                GrimoireList[4].SetActive(true);
+                break;
+            case "foudre":
+                GrimoireList[5].SetActive(true);
+                break;
+            case "power":
+                GrimoireList[6].SetActive(true);
+                break;
+            case "soin":
+                GrimoireList[7].SetActive(true);
+                break;
+        }
+    }
+
+    public void OnClicEvent(string button)
+    {
+        switch (button)
+        {
+            case "Continue":
+                GameManager.Instance.ChangeGameState("InGame");
+                break;
+            case "Save":
+
+                break;
+            case "MainMenu":
+                GameManager.Instance.ChangeGameState("MainMenu");
+                break;
+        }
+    }
+}
