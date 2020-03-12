e4d543dbf5cba8fabd92c0289f88eba179540efa
QuentinLamy <quentin.lamy16@gmail.com>
2020-03-12 15:34:06 +0100

    j'ai essayé le piège de glace ok

---
+++ Typing/Assets/Scripts/IceTrap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTrap : MonoBehaviour
+{
+    Animator animator;
+
+    private bool flag = false;
+    private float timer = 0;
+
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(flag)
+        {
+            timer++;
+            if(timer == 60)
+            {
+                animator.speed = 0;
+            }
+            else if(timer >= 240)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("Contact", true);
+        var PEGU = collision.gameObject.GetComponent<Transform>();
+        flag = true;
+        PEGU.position = this.transform.position;
+    }
+}
